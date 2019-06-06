586a6f0ac3fd8521a6cee005e174651653a7969d
Drake Bott <bott.da@gmail.com>
2019-06-06 16:47:52 -0600

    Update import path

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import '../widgets/bitsmarter_reusablewidgets.dart';
+import '../bitsmarter_reusablewidgets.dart';
 
 
 class HomePage extends StatelessWidget {
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
